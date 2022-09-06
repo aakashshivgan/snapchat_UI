1daa9150fe66a8ed7bb58dabb3d30fada6fac0b8
Admin <aakashshivgan7739@gmail.com>
2022-09-06 12:04:38 +0530

    created auth screen

--- lib/Screen/auth_screen.dart
+++ lib/Screen/auth_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:snapchat_ui/Screen/login_screen.dart';
+import 'package:snapchat_ui/Screen/register_screen.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -13,10 +15,63 @@ class _AuthScreenState extends State<AuthScreen> {
     return Scaffold(
       backgroundColor: Colors.yellowAccent,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset("assets/snaplogo.png"),
+          SizedBox(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 150, vertical: 200.5),
+              child: Image.asset(
+                "assets/snapchat.png",
+                height: 120,
+                width: 120,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 82.5,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()));
+            },
+            child: Container(
+              color: Colors.redAccent,
+              width: double.infinity,
+              height: 100,
+              child: const Center(
+                child: Text(
+                  "LOG IN",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const RegisterScreen()));
+            },
+            child: Container(
+              color: Colors.lightBlue,
+              width: double.infinity,
+              height: 100,
+              child: const Center(
+                child: Text(
+                  "SIGN UP",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
           )
         ],
       ),
