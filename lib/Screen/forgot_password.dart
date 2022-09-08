8fce743b967345e23ebe844532b8e30fdd7c2971
Admin <aakashshivgan7739@gmail.com>
2022-09-08 13:50:30 +0530

    Forgot password page complete

--- lib/Screen/forgot_password.dart
+++ lib/Screen/forgot_password.dart
@@ -1,14 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:snapchat_ui/widgets/signup_login_btn.dart';
+import 'package:snapchat_ui/widgets/txt_fields.dart';
 
-class ForgotPasswordScreen extends StatelessWidget {
+class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({Key? key}) : super(key: key);
 
   @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String email = "";
+  String error = "";
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("forgotPassword"),
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.only(top: 40, bottom: 50),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+                color: Colors.blue,
+                size: 35,
+              ),
+            ),
+          ),
+          Container(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  const Text(
+                    "Enter your email",
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: TextFieldsForScreen(
+                        label: "Email",
+                        txtType: TextInputType.emailAddress,
+                        onChange: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+                        validator: ((val) =>
+                            val.isempty ? "Enter an email" : null),
+                        obscure: false),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    error,
+                    style: const TextStyle(
+                      color: Colors.red,
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SignupLoginBtn(
+              color: Colors.blue,
+              onPress: () async {
+                if (_formKey.currentState!.validate()) ;
+              },
+              text: "Submit")
+        ],
       ),
-    );
+    ));
   }
 }
